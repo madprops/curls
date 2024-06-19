3a369fc586c38197cd5100bef41f597625360237
Auric Vente <mprops@protonmail.com>
2024-06-19 09:30:08 -0600

    Mods

---
+++ utils/tag.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require "git"
+require "json"
+file = File.read("manifest.json")
+manifest = JSON.parse(file)
+version = manifest["version"]
+name = "v#{version}"
+puts name
+repo = Git.open(".")
+repo.add_tag(name)
+repo.push("origin", name)
+puts "Created tag: #{name}"
