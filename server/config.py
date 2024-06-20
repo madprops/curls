9550a195ace51150a8f5db95aacd3426c68a1f4e
Auric Vente <mprops@protonmail.com>
2024-06-20 14:07:06 -0600

    Mods

--- server/config.py
+++ server/config.py
@@ -6,17 +6,17 @@ curl_max_length = 18
 passkey_length = 18
 content_max_length = 500
 max_curls = 100
+captcha_key = "changeMe"
 
 with open("captcha_key.txt", "r") as f:
     captcha_key = f.read().strip()
-    print(captcha_key)
 
-    captcha = {
-        "SECRET_CAPTCHA_KEY": captcha_key,
-        "CAPTCHA_LENGTH": 10,
-        "CAPTCHA_DIGITS": False,
-        "EXPIRE_SECONDS": 60,
-        "CAPTCHA_IMG_FORMAT": "JPEG",
-        "ONLY_UPPERCASE": False,
-        "CHARACTER_POOL": string.ascii_lowercase,
-    }
+captcha = {
+    "SECRET_CAPTCHA_KEY": captcha_key,
+    "CAPTCHA_LENGTH": 10,
+    "CAPTCHA_DIGITS": False,
+    "EXPIRE_SECONDS": 60,
+    "CAPTCHA_IMG_FORMAT": "JPEG",
+    "ONLY_UPPERCASE": False,
+    "CHARACTER_POOL": string.ascii_lowercase,
+}
