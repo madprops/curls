6628f7e7b6f37c10405b05e55dbf2cb8a72ea43f
Auric Vente <mprops@protonmail.com>
2024-07-05 07:52:03 -0600

    Mods

--- utils/count.sh
+++ utils/count.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
 # Count the total number of lines
-cd server/static/dashboard/js/main/
+
+cd server/
+echo "Python:"
+wc -l app.py procs.py db.py bundle.py | awk 'END {print $1}'
+
+cd static/dashboard/js/main/
+echo "Javascript:"
 find . -type f -name "*.js" -exec wc -l {} \; | awk '{t+=$1} END {print t}'
