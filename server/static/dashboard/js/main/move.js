7592f9fccc9b5e53119e4ffd2bc96a8724d66852
Auric Vente <mprops@protonmail.com>
2024-07-12 00:32:48 -0600

    Mods

--- server/static/dashboard/js/main/move.js
+++ server/static/dashboard/js/main/move.js
@@ -9,15 +9,12 @@ class Move {
         let items = Container.get_visible()
         let selected = Select.get()
         let first_index = items.indexOf(selected[0])
-        let prev
 
-        if (first_index === 0) {
-            prev = items[first_index]
-        }
-        else {
-            prev = items[first_index - 1]
+        if (first_index > 0) {
+            first_index -= 1
         }
 
+        let prev = items[first_index]
         prev.before(...selected)
         Utils.scroll_element({item: selected[0]})
         this.save()
@@ -27,15 +24,12 @@ class Move {
         let items = Container.get_visible()
         let selected = Select.get()
         let last_index = items.indexOf(Utils.last(selected))
-        let next
 
-        if (last_index === items.length - 1) {
-            next = items[last_index]
-        }
-        else {
-            next = items[last_index + 1]
+        if (last_index < (items.length - 1)) {
+            last_index += 1
         }
 
+        let next = items[last_index]
         next.after(...selected)
         Utils.scroll_element({item: Utils.last(selected)})
         this.save()
