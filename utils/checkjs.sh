c18f812bba6fb11d1c110034b2876d6bc1a912a4
Auric Vente <mprops@protonmail.com>
2025-12-12 11:35:01 -0600

    Mods

--- utils/checkjs.sh
+++ utils/checkjs.sh
@@ -2,12 +2,12 @@
 export NODE_OPTIONS="--no-warnings"
 
 # Only check files that have changed recently
-# last_tag=$(git describe --tags --abbrev=0)
+last_tag=$(git describe --tags --abbrev=0)
 
 # Pick one
-files=$(git diff --name-only $last_tag HEAD -- '*.js')
-# files=$(git ls-files -- "*.js")
-files=$(echo $files | tr " " "\n" | grep -v "/libs/" | grep -v "/ace/" | tr "\n" " ")
+# files=$(git diff --name-only $last_tag HEAD -- '*.js')
+files=$(git ls-files -- "*.js")
+files=$(echo $files | tr " " "\n" | grep -vE '(^|/)dist/' | grep -vE '(^|/)vite.config.js$' | grep -vE '(^|/)libs/*' | tr "\n" " ")
 
 if [ -n "$files" ]; then
   npm run --silent lint $files
--- utils/fixjs.sh
+++ utils/fixjs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 files=$(git ls-files -- "*.js")
-files=$(echo $files | tr " " "\n" | grep -v "/libs/" | grep -v "/ace/" | tr "\n" " ")
+files=$(echo $files | tr " " "\n" | grep -vE '(^|/)dist/' | grep -vE '(^|/)vite.config.js$' | grep -vE '(^|/)libs/*' | tr "\n" " ")
 
 if [ -n "$files" ]; then
   npm run --silent fix $files
